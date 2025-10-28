a7fa4c127f31942ae31a7cf02e89cb51b2c66639
Pablo Rubinstein <prubinst@dc.uba.ar>
2025-10-28 18:56:14 -0300

    agregue un caso de test que vale la pena

--- test/test_points_to.py
+++ test/test_points_to.py
@@ -2,39 +2,34 @@ import random
 import unittest
 
 from pyDatalog import pyDatalog
+from src.points_to import PointsToAnalyzer
 
-
-# los terminos de datalog solo pueden ser definidos a nivel de modulo :-/
-pyDatalog.create_terms("PointsTo, New, Assign, X, Y, O")
+JAVA_SNIPPET = """
+class T {
+  void m() {
+    A x = new A();
+    A y = new A();
+    x.f = y;
+    A z = x.f;
+    A xx;
+    xx = y;
+  }
+}
+"""
 
 
 class PointsToTester(unittest.TestCase):
-    def test_order(self):
-        """Chequea que el orden en que se pasan los facts no altera el resultado"""
-        facts = [
-            "+New('a', 'obj1')",
-            "+New('b', 'obj2')",
-            "+Assign('b', 'a')",
-            "+Assign('a', 'b')",
-            "+Assign('c', 'b')",
-            "+Assign('c', 'a')"
-        ]
+    def test_extract_facts(self):
+        with open('test.java', 'w') as f:
+            f.write(JAVA_SNIPPET)
 
-        random.seed(123456)
-        results = []
-        for i in range(10):
-            pyDatalog.clear()
-            PointsTo(X, O) <= New(X, O)
-            PointsTo(X, O) <= Assign(X, Y) & PointsTo(Y, O)
-            for fact in random.sample(facts, k=len(facts)):
-                eval(fact)
-  
-            result = set(PointsTo(X, O).data)
-            results.append(result)
+        pta = PointsToAnalyzer()
+        new_facts, assign_facts, store_facts, load_facts = pta.extract_facts('test.java')
 
-        # comparamos que todos sean iguales al primero (es decir, todos iguales entre si)
-        for i in range(1, 10):
-            self.assertEqual(results[0], results[i])
+        self.assertEqual([('x', 'Obj0'), ('y', 'Obj1')], new_facts)
+        self.assertEqual([('xx', 'y')], assign_facts)
+        self.assertEqual([('x', 'f', 'y')], store_facts)
+        self.assertEqual([('z', 'x', 'f')], load_facts)
 
 
 if __name__ == '__main__':
