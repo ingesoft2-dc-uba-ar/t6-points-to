a8410780253dd9ff4eaf8ea94f5386c0dfb7c48a
Pablo Rubinstein <prubinst@dc.uba.ar>
2025-10-28 18:55:31 -0300

    no muestro las soluciones

--- src/abuelo.py
+++ src/abuelo.py
@@ -12,35 +12,3 @@ for a, b in Abuelo(X, Z).data:
 
 # no llamo a clear() -> siguen valiendo las reglas y hechos anteriores
 
-pyDatalog.create_terms('Bisabuelo')
-
-Bisabuelo(X, Y) <= Padre(X, Z) & Abuelo(Z, Y)
-
-+Padre("Esteban", "Alberto")
-+Padre("Esteban", "Blas")
-+Padre("Alberto", "Oscar")
-
-print()
-for a, b in Bisabuelo(X, Y).data:
-    print(f"{a} es bisabuelo de {b}")
-
-pyDatalog.create_terms('Familiar')
-
-Familiar(X, Y) <= Padre(X, Y)
-Familiar(X, Y) <= Padre(Y, X)
-Familiar(X, Y) <= Familiar(X, Z) & Familiar(Z, Y)
-
-print()
-for a, b in Familiar(X, Y).data:
-    print(f"{a} es familiar de {b}")
-
-pyDatalog.create_terms('Antepasado')
-
-Antepasado(X, Y) <= Padre(X, Y)
-Antepasado(X, Y) <= Antepasado(X, Z) & Antepasado(Z, Y)
-
-print()
-for a, b in Antepasado(X, Y).data:
-    print(f"{a} es antepasado de {b}")
-
-
